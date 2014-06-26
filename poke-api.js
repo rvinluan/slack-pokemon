2e6873fe552497c432765cc240dcf7a8f7fea8e6
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-25 23:50:16 -0400

    on end

--- poke-api.js
+++ poke-api.js
@@ -4,10 +4,15 @@ module.exports = {}
 
 module.exports.getPokemon = function(name, callback) {
   http.get("http://pokeapi.co/api/v1/pokemon/"+name, function(res){
+    console.log("Got response: " + res.statusCode);
     res.setEncoding('utf-8');
+    body = '';
     res.on("data", function(chunk) {
-      console.log(chunk);
-      callback.call(this, chunk);
+      body += chunk;
+    })
+    res.on("end", function(){
+      console.log(body);
+      // callback.call(this, JSON.parse(body));
     })
   }).on('error', function(e) {
     console.log("got error: " + e.message)
