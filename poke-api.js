f790ed0ebae9aa654e92342d20f9532fb4b0fef2
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-25 23:45:06 -0400

    typo

--- poke-api.js
+++ poke-api.js
@@ -3,11 +3,11 @@ var http = require('http')
 module.exports = {}
 
 module.exports.getPokemon = function(name, callback) {
-  http.get("http://pokeapi.co/api/v1/pokemon"+name, function(res){
+  http.get("http://pokeapi.co/api/v1/pokemon/"+name, function(res){
     res.setEncoding('utf-8');
     res.on("data", function(chunk) {
       console.log(res.statusCode);
-      console.log(name);
+      console.log("the name was: " + name);
       console.log(chunk);
       callback.call(this, chunk);
     })
