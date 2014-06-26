73f609fe4ffb1211b30814e08ea48a4ac59c6f20
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-26 13:34:21 -0400

    did it\!

--- poke-api.js
+++ poke-api.js
@@ -4,16 +4,14 @@ module.exports = {}
 
 module.exports.getPokemon = function(name, callback) {
   request("http://pokeapi.co/api/v1/pokemon/"+name, function (error, response, body) {
-    console.log(response.statusCode);
     if (response.statusCode == 404) {
-      console.log('404 bro')
       callback.call(this, {"error": "failed to get pokemon"});
     }
     else if (response.statusCode == 200) {
       callback.call(this, JSON.parse(body));
     }
     else {
-      callback.call(this, JSON.parse({"error": "weird error"}))
+      callback.call(this, {"error": "weird error"})
     }
   })
 }
