fa3c5c760d51a6ea80bad6554d81727dc9368e3f
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-26 00:06:53 -0400

    use request library to handle redirects

--- poke-api.js
+++ poke-api.js
@@ -1,17 +1,11 @@
-var http = require('http')
+var request = require('request')
 
 module.exports = {}
 
 module.exports.getPokemon = function(name, callback) {
-  http.get("http://pokeapi.co/api/v1/pokemon/"+name, function(res){
-    console.log("Got response: " + res.statusCode);
-    res.setEncoding('utf-8');
-    body = '';
-    res.on("data", function(chunk) {
-      console.log("fetching")
-      body += chunk;
-    });
-  }).on('error', function(e) {
-    console.log("got error: " + e.message)
+  request("http://pokeapi.co/api/v1/pokemon/"+name, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body) // Print the google web page.
+    }
   })
 }
