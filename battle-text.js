42b485f38dacfa154ef76ff02967cee05d831a65
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-26 10:54:58 -0400

    man, function names

--- battle-text.js
+++ battle-text.js
@@ -3,7 +3,7 @@ var pokeapi = require('./poke-api.js')
 module.exports = {}
 
 //Response to an unrecognized command
-module.exports.unrecognizedText = function(commandsArray) {
+module.exports.unrecognizedCommand = function(commandsArray) {
   var textString = "I don't recognize that command _command_ . Try <http://rv-slack-pokemon.herokuapp.com> for help.";
   return { "text": textString }
 }
