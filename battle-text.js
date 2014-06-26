6dc43265a2a0f8c8750d0930b301c24fee346dbf
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-26 12:36:49 -0400

    log

--- battle-text.js
+++ battle-text.js
@@ -18,6 +18,7 @@ module.exports.choosePokemon = function(commandsArray, callback) {
     module.exports.unrecognizedCommand(commandsArray, callback);
     return;
   }
+  console.log('trying to fetch' + pokemonName);
   //grab the pokemon's data from the API
   pokeapi.getPokemon(pokemonName, function(data){
     console.log(data)
