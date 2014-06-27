6880f4bfb20da225a9321c9682b894b06ea83900
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 03:28:09 -0400

    test

--- move-types.js
+++ move-types.js
@@ -2,11 +2,5 @@ var fs = require('fs');
 
 module.exports.getMoveType = function(moveName) {
   var data = fs.readFileSync("./scraped_json/scrape_output.json");
-  data = data.filter(function(elem, index, arr){
-    if(moveName in elem) {
-      return elem[moveName];
-    } else {
-      return "";
-    }
-  })
+  console.log(data[0]);
 }
