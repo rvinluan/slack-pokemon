b463393776abf0d3dbcfef668fd8874c22056755
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 12:17:30 -0400

    uh

--- state-machine.js
+++ state-machine.js
@@ -52,12 +52,9 @@ module.exports.endBattle = function(callback) {
 module.exports.addMove = function(data, callback) {
   console.log("about to put in the move "+data.name.toLowerCase())
   redis.sadd("user:allowedMoves", data.name.toLowerCase());
-  redis.hmset("move:"+data.name.toLowerCase(), {
-    "power": data.power,
-    "type": moves.getMoveType(data.name.toLowerCase())
-  }, function(data){
+  redis.hmset("move:"+data.name.toLowerCase(), "power", data.power , "type", moves.getMoveType(data.name.toLowerCase()), function(d){
     console.log('this should mean hmset is done.');
-    callback(data)
+    callback(d)
   })
 }
 
