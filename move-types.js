a1e415d69ae9e81a44d07888af9dad7ea3835be3
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 03:32:14 -0400

    t

--- move-types.js
+++ move-types.js
@@ -2,5 +2,5 @@ var fs = require('fs');
 
 module.exports.getMoveType = function(moveName) {
   var data = fs.readFileSync("./scraped_json/scrape_output.json");
-  console.log("first move:" + data[2]);
+  console.log("data start:" + data);
 }
