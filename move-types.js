e49734d04472332efed7ea6ceade36147d8600cd
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 03:23:50 -0400

    typo

--- move-types.js
+++ move-types.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
 
-module.exports.getMoveType(moveName) {
+module.exports.getMoveType = function(moveName) {
   var data = fs.readFileSync("./scraped_json/scrape_output.json");
   data = data.filter(function(elem, index, arr){
     if(moveName in elem) {
