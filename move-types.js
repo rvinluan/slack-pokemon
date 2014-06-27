65769d88b2407e0473e06d0953e7ed1464eedb81
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 03:57:41 -0400

    crossing fingers

--- move-types.js
+++ move-types.js
@@ -4,13 +4,11 @@ module.exports.getMoveType = function(moveName) {
   var data = fs.readFileSync("./scraped_json/scrape_output.json");
   data = JSON.parse(data);
   data = data.filter(function(val, index, arr){
-    console.log("current data object: "+Object.keys(val)[0]);
-    console.log("move name: "+moveName);
-    return true;
+    return Object.keys(val)[0] == moveName;
   });
   if(data.length > 0) {
     return data[0][moveName];
   } else {
-    return "unavailable";
+    return "something weird";
   }
 }
