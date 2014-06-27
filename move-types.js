919955ff0a2069362555fb11390975eda19d465b
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 03:46:40 -0400

    real

--- move-types.js
+++ move-types.js
@@ -3,11 +3,12 @@ var fs = require('fs');
 module.exports.getMoveType = function(moveName) {
   var data = fs.readFileSync("./scraped_json/scrape_output.json");
   data = JSON.parse(data);
-  data.filter(function(val, index, arr){
-    if (moveName in val) {
-      return val[moveName];
-    } else {
-      return "unavailable";
-    }
-  })
+  data = data.filter(function(val, index, arr){
+    return (moveName in val)
+  });
+  if(data.length > 0) {
+    return data[0][moveName];
+  } else {
+    return "unavailable";
+  }
 }
