fa3da275bb819d13aa557b873ea704d498d19298
Robert Vinluan <robert.vinluan@gmail.com>
2014-06-27 03:55:11 -0400

    syntax

--- move-types.js
+++ move-types.js
@@ -4,7 +4,7 @@ module.exports.getMoveType = function(moveName) {
   var data = fs.readFileSync("./scraped_json/scrape_output.json");
   data = JSON.parse(data);
   data = data.filter(function(val, index, arr){
-    console.log("current data object: "+val.keys()[0]);
+    console.log("current data object: "+Object.keys(val)[0]);
     console.log("move name: "+moveName);
     return true;
   });
