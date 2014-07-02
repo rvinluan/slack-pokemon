1bca56ff16a068577bbdffe82a517395fe576fc7
Robert Vinluan <robert.vinluan@gmail.com>
2014-07-02 17:01:39 -0400

    fix warning with body-parser

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded())
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(request, response) {
   response.send('Hello There!')
